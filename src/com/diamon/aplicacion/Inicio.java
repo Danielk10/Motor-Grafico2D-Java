22db5d654d633906e2767b4adf0848995ecdd436
Danielk10 <danielpdiamon@gmail.com>
2022-03-15 17:50:15 -0430

    primera version

---
+++ src/com/diamon/aplicacion/Inicio.java
@@ -0,0 +1,12 @@
+package com.diamon.aplicacion;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hola");
+
+	}
+
+}
---
+++ src/com/diamon/nucleo/Juego.java
@@ -0,0 +1,20 @@
+package com.diamon.nucleo;
+
+import java.awt.Canvas;
+import java.awt.GraphicsConfiguration;
+
+public abstract class Juego extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+
+	public Juego() {
+
+	}
+
+	public Juego(GraphicsConfiguration configuracion) {
+
+		super(configuracion);
+
+	}
+
+}
